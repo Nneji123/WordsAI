21425b60d3491197547f81fc15b1da4b9ecf93f3
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-08-01 12:34:23 +0000

    added sentiment analyzer

--- app.py
+++ app.py
@@ -1,1 +1,66 @@
-template
+from fastapi import FastAPI
+from fastapi.responses import FileResponse, PlainTextResponse
+from fastapi.middleware.cors import CORSMiddleware
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+
+app = FastAPI(
+    title="WordsAI API",
+    description="""A collection of NLP Applications served as APIs using FastAPI.""",
+    version="0.0.1",
+    debug=True
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+favicon_path = "./images/favicon.ico"
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
+
+
+@app.get("/", response_class=PlainTextResponse, tags=["home"])
+async def home():
+    note = """
+WordsAI API 📚
+A collection of NLP Applications served as APIs using FastAPI!
+Note: add "/redoc" to get the complete documentation.
+  """
+    return note
+
+# Sentiment Route
+@app.post("/sentiment")
+async def get_sentiment(text: str) -> str:
+    """
+    The get_sentiment function accepts a string as an argument and returns the sentiment of that string.
+    The function uses the VADER SentimentIntensityAnalyzer to determine if the text is positive, negative, or neutral.
+    
+    Args:
+        text:str: Pass in the text that is being analyzed
+    
+    Returns:
+        A string that is either positive, negative or neutral
+
+    """
+    analyzer = SentimentIntensityAnalyzer()
+    result = analyzer.polarity_scores(text)
+    sentiment = None
+    if result["compound"] >= 0.05:
+        sentiment = "Positive"
+    elif result["compound"] <= -0.05:
+        sentiment = "Negative"
+    else:
+        sentiment = "Neutral"
+    return f"The sentiment of the text is: {sentiment} and the Score is: {round(result['compound'], 2)}"
+
+
