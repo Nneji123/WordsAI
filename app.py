5cfd241910b47f0d06193db6391aaec553959857
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-08-01 13:01:50 +0000

    added summary

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse, PlainTextResponse
 from fastapi.middleware.cors import CORSMiddleware
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from pysummarization.web_scraping import WebScraping
+from pysummarization.abstractabledoc.std_abstractor import StdAbstractor
 from pysummarization.nlpbase.auto_abstractor import AutoAbstractor
 from pysummarization.tokenizabledoc.simple_tokenizer import SimpleTokenizer
 from pysummarization.abstractabledoc.top_n_rank_abstractor import TopNRankAbstractor
+from pysummarization.nlpbase.auto_abstractor import AutoAbstractor
 
 
 app = FastAPI(
@@ -74,14 +77,14 @@ async def get_summary(text: str) -> dict:
     The get_summary function accepts a string of text and returns a dictionary with the following keys:
         - summary_list: A list of strings containing the most important sentences in the document.
         - title_string: The first sentence in the document, which is usually used as a title.
-    
-    
+
+
     Args:
         text:str: Pass in the text that is to be summarized
-    
+
     Returns:
         A dictionary with the following keys:
-    
+
     """
     auto_abstractor = AutoAbstractor()
 
@@ -92,4 +95,43 @@ async def get_summary(text: str) -> dict:
     abstractable_doc = TopNRankAbstractor()
 
     result_dict = auto_abstractor.summarize(text, abstractable_doc)
+    return result_dict
+
+@app.post("/summarize_webpage")
+async def get_summary_webpage(url):
+    """
+    The get_summary_webpage function scrapes a webpage and summarizes it.
+    It returns a dictionary with the following keys:
+        - summary_list: A list of strings containing the most important sentences in the document.
+        - title_string: The first sentence in the document, which is usually used as a title.
+    Args:
+        url: Specify the url of the website to be summarized
+
+    Returns:
+        The summarized text
+    """
+    # Object of web scraping.
+    web_scrape = WebScraping()
+    # Web-scraping.
+    document = web_scrape.scrape(url)
+
+    # Object of automatic summarization.
+    auto_abstractor = AutoAbstractor()
+    # Set tokenizer.
+    auto_abstractor.tokenizable_doc = SimpleTokenizer()
+    # Set delimiter.
+    auto_abstractor.delimiter_list = [".", "\n"]
+    # Object of abstracting and filtering document.
+    abstractable_doc = TopNRankAbstractor()
+    # Summarize document.
+    result_dict = auto_abstractor.summarize(document, abstractable_doc)
+
+    # Output 3 summarized sentences.
+    limit = 3
+    i = 1
+    for sentence in result_dict["summarize_result"]:
+        print(sentence)
+        if i >= limit:
+            break
+        i += 1
     return result_dict
