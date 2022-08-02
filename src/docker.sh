65d6bd278065580018b95a7c27235f2ce1cb04b6
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-08-02 22:56:22 +0000

    updates

--- src/docker.sh
+++ src/docker.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-pip install chatterbot==1.0.4
-pip install spacy==2.3.5 
-pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.3.1/en_core_web_sm-2.3.1.tar.gz 
-pip install pyresparser 
+
 
 python -m spacy download en_core_web_sm 
 python -m nltk.downloader words 
