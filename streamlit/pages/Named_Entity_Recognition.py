505772826a29612adcbef08e78ae19c921889449
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-08-02 15:49:13 +0000

    will update streamlit later

--- streamlit/pages/Named_Entity_Recognition.py
+++ streamlit/pages/Named_Entity_Recognition.py
@@ -10,13 +10,13 @@ st.image("./images/logo.png")
 
 st.write("""
 ## About
-This sentiment analyzer app uses the WordsAI API to get the sentiments of a text
+This Named Entity Recognition app uses the WordsAI API to recognize named entities in a text
 """)
 
 texts = st.text_input("Input text: ")
 # make a post request to the fastapi server
 
-if st.button ("Get sentiment"):
+if st.button ("Get Named Entities"):
     response = re.post("http://wordsai-api.herokuapp.com/named_entity_recognition"+"?text="+texts)
     response_json = response.json()
     st.write(response_json)
