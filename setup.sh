e0e87593ca35cb703ca3ea7591b2a5830222d2dc
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-08-02 12:01:58 +0000

    updated fikles

--- setup.sh
+++ setup.sh
@@ -8,5 +8,11 @@ python -m spacy download en_core_web_sm
 python -m nltk.downloader words 
 python -m nltk.downloader stopwords 
 python -m nltk.downloader punkt 
+python -m nltk.downloader wordnet
+python -m nltk.downloader averaged_perceptron_tagger
+python -m nltk.downloader maxent_treebank_pos_tagger
+python -m nltk.downloader punkt_tokenizer
+python -m nltk.downloader universal_tagset
+
 
 gunicorn -w 3 -k uvicorn.workers.UvicornWorker app:app --bind 0.0.0.0:$PORT
