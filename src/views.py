1b6aac8fcb5987c567f2c58d0b3a45228ee7dc29
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-08-02 22:22:40 +0000

    updated html files

--- src/views.py
+++ src/views.py
@@ -7,6 +7,11 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
 
 @app.get("/translation")
 def home(request: Request):
