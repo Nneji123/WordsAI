3d0a9fb37290737a4019f707001d461f2ce0222f
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-08-04 15:09:01 +0100

    Update setup.sh

--- .github/workflows/scripts/setup.sh
+++ .github/workflows/scripts/setup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+sudo apt-get -y update  && sudo apt-get install -y \
+  python3-dev \
+  apt-utils \
+  python-dev \
+  tesseract-ocr \
+  portaudio19-dev \
+  swig \
+  libpulse-dev \
+  pocketsphinx \
+
 pip install chatterbot==1.0.4
 pip install spacy==2.3.5 
 pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.3.1/en_core_web_sm-2.3.1.tar.gz 
