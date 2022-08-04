9aaf5b7784bb3769d1d63c9dbbe731b1548e707e
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-08-04 09:48:54 +0000

    added text2speech files and tests, function works as expected

--- src/app.py
+++ src/app.py
@@ -150,11 +150,6 @@ async def home(request: Request):
     )
 
 
-@app.get("/chatbot")
-def home(request: Request):
-    return templates.TemplateResponse("chatbot.html", {"request": request})
-
-
 @app.get("/resume")
 def home(request: Request):
     return templates.TemplateResponse("resume.html", {"request": request})
@@ -304,4 +299,24 @@ async def home(request: Request):
 
     return templates.TemplateResponse(
         "profanity.html", {"request": request, "message": text, "sumary": sumary}
+    )
+
+@app.get("/text_to_speech")
+def home(request: Request):
+    return templates.TemplateResponse("text2speech.html", {"request": request})
+
+@app.post("/text2speech")
+async def home(request: Request):
+    sumary = ""
+    if request.method == "POST":
+        form = await request.form()
+        if form["message"] and form["language"]:
+            language = form["language"]
+            text = form["message"]
+            translate = text2speech(language, text)
+            sumary = " ".join(translate)
+
+    return templates.TemplateResponse(
+        "text2speech.html",
+        {"request": request, "message": text, "language": language, "sumary": sumary},
     )
